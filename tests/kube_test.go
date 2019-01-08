2ef073b328b6e4846a8def74108289a67c117146
omarvides <omarvides@gmail.com>
2019-01-08 00:03:50 -0600

    Resolves #1: Adding structure to go tests

--- tests/kube_test.go
+++ tests/kube_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/random"
@@ -12,7 +14,13 @@ func TestKubernetes(t *testing.T) {
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../gcp",
 		Vars: map[string]interface{}{
-			"something": random.UniqueId(),
+			"cluster_name":         fmt.Sprintf("default-%s", random.UniqueId()),
+			"json_cred_file":       os.Getenv("CRED_FILE"),
+			"master_node_password": os.Getenv("MASTER_PASS"),
+			"master_node_username": "admin",
+			"project_id":           os.Getenv("PROJ_ID"),
+			"node_pool_name":       "default",
+			"zone":                 "us-central1-a",
 		},
 	}
 	defer terraform.Destroy(t, terraformOptions)
